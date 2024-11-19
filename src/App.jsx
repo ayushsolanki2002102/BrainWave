ccbb33ddd1309b0bfca82a02978a1d48974caa9d
Ayush Solanki <ayushsolanki2002102@gmail.com>
2024-11-19 16:55:12 +0530

    Header WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-    <h1>Hey ayush</h1>
-    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Button className="mt-10" href="#login">
-        lalalala
-      </Button>
+      
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header/>
+      </div>
 
-
-    </div>
-    <ButtonGradient/>
+      <ButtonGradient />
     </>
-  )
-}
+  );
+};
 
 export default App;
