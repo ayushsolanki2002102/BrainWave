77083cd6f914350d497dce7a306b5f1bb4fe07c1
Ayush Solanki <ayushsolanki2002102@gmail.com>
2024-11-19 11:55:44 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <>
+    <h1>Hey ayush</h1>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Button className="mt-10" href="#login">
+        
+      </Button>
+
+
+    </div>
+    <ButtonGradient/>
+    </>
+  )
+}
+
+export default App;
